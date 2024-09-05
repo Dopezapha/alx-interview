a1fed666bba0ec452fbd8011dc173326c1094cbb
Dopezapha <officialnwaneridaniel@gmail.com>
2024-09-05 09:43:19 -0400

    Star Wars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+
+const request = require('request');
+const API_URL = 'https://swapi-api.alx-tools.com/api';
+
+if (process.argv.length > 2) {
+  const movieId = process.argv[2];
+  request(`${API_URL}/films/${movieId}/`, (err, response, body) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      console.error(`Error: Status code ${response.statusCode}`);
+      return;
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, characterResponse, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+            return;
+          }
+          if (characterResponse.statusCode !== 200) {
+            reject(`Error: Status code ${characterResponse.statusCode}`);
+            return;
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.error(allErr));
+  });
+} else {
+  console.error('Please provide a movie ID');
+}
